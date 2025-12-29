5b468c2fb57bb817179b9593973d25b1bf30d3cf
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2025-12-29 17:19:42 +0900

    TLNG-004 : [Task] Implement card selection and cancellation

--- Assets/Scripts/Actor/Hand.cs
+++ Assets/Scripts/Actor/Hand.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Actor
 {
@@ -7,10 +9,14 @@ namespace Actor
     {
         public GameObject cardPrefab;
 
-        private float spawnDelay = 0.2f; 
+        private float spawnDelay = 0.2f;
+
+        private int startNum;
+        private List<int> selectedNumbers = new List<int>();
 
         public IEnumerator CreateHand(int curNum)
         {
+            startNum = curNum;
             for (int i = 0; i < 3; i++)
             {
                 CreateCard(curNum + i);
@@ -26,6 +32,56 @@ namespace Actor
             {
                 cardScript.SetNumber(num);
             }
+            Button btn = newCard.GetComponent<Button>();
+            if (btn != null)
+            {
+                btn.onClick.AddListener(() => OnCardSelected(num, newCard, btn));
+            }
+        }
+
+        private void OnCardSelected(int clickedNum, GameObject cardObj, Button btn)
+        {
+            if (selectedNumbers.Contains(clickedNum))
+            {
+                HandleCancel(clickedNum, cardObj, btn);
+            }
+            else
+            {
+                HandleSelect(clickedNum, cardObj, btn);
+            }
+        }
+
+        private void HandleSelect(int clickedNum, GameObject cardObj, Button btn)
+        {
+            bool isValid = (selectedNumbers.Count == 0 && clickedNum == startNum) ||
+                           (selectedNumbers.Count > 0 && clickedNum == selectedNumbers[^1] + 1);
+
+            if (isValid)
+            {
+                selectedNumbers.Add(clickedNum);
+                SetCardVisual(cardObj, true);
+            }
+            else
+            {
+            }
+        }
+
+        private void HandleCancel(int clickedNum, GameObject cardObj, Button btn)
+        {
+            if (selectedNumbers[^1] == clickedNum)
+            {
+                selectedNumbers.RemoveAt(selectedNumbers.Count - 1);
+                SetCardVisual(cardObj, false); 
+            }
+            else
+            {
+            }
+        }
+
+        private void SetCardVisual(GameObject cardObj, bool isSelected)
+        {
+            var image = cardObj.GetComponent<Image>();
+            image.color = isSelected ? Color.gray : Color.white;
         }
     }
 }
