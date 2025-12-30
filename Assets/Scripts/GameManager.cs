23cb85f9593f72656234f5005054c1b290f4fd41
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2025-12-30 15:20:08 +0900

    TLNG-011 : [Task] Implement automatic GameManager generation across all scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,29 @@
 using Round;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     private static int totalRound = 10;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize()
+    {
+        GameObject go = new GameObject("GameManager");
+        Instance = go.AddComponent<GameManager>();
+        DontDestroyOnLoad(go);
+    }
+
     void Awake()
     {
     }
 
     private void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+
         //for (int i = 0; i < 3; i++)
         //{
         //    RoundManager.StartRound(i + 1);
@@ -25,6 +39,9 @@ public class GameManager : MonoBehaviour
         //    RoundManager.StartRound(i + 1);
         //}
 
-        RoundManager.StartRound(10);
+        if (currentScene.name == "GameScene")
+        {
+            RoundManager.StartRound(10);
+        }
     }
 }
